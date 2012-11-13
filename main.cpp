e02ba1dcc92c332741507b02cf951e403c9c1b9b
Matthew Peveler <matt.peveler@gmail.com>
2012-11-13 09:28:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,28 +11,33 @@ int main(int argc, const char* argv[])
 
 	nodeArray[0] = new BNode("Population");
 	nodeArray[0]->setArity(3);
+
 	nodeArray[1] = new BNode("Time of Day");
 	nodeArray[1]->setArity(5);
+
 	nodeArray[2] = new BNode("Lighting");
 	nodeArray[2]->setArity(5);
+
 	nodeArray[3] = new BNode("Drugs");
 	nodeArray[3]->setArity(5);
+
 	nodeArray[4] = new BNode("Location At");
 	nodeArray[4]->setArity(3);
+
 	nodeArray[5] = new BNode("Location From");
 	nodeArray[5]->setArity(3);
+
 	nodeArray[6] = new BNode("Murder");
-	nodeArray[6]->setArity();
 	nodeArray[7] = new BNode("Robbery");
-	nodeArray[7]->setArity();
 	nodeArray[8] = new BNode("Rape");
-	nodeArray[8]->setArity();
 
 	for(int i = 0; i < 6; i++)
 		for(int j = 6; j < 9; j++)
 			nodeArray[i]->setChild(nodeArray[j]);
 
-
+	nodeArray[6]->setArity();
+	nodeArray[7]->setArity();
+	nodeArray[8]->setArity();
 
 	return 0;
 }
