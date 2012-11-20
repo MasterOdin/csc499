737628d583a9c60fafa1b6b2475d6d3db0192915
Matthew Peveler <matt.peveler@gmail.com>
2012-11-20 11:50:09 -0500

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -11,27 +11,6 @@ int main(int argc, const char* argv[])
 
 	nodeArray[0] = new BNode("Population","population");
 	nodeArray[0]->setArity(3);
-<<<<<<< HEAD
-
-	nodeArray[1] = new BNode("Time of Day");
-	nodeArray[1]->setArity(5);
-
-	nodeArray[2] = new BNode("Lighting");
-	nodeArray[2]->setArity(5);
-
-	nodeArray[3] = new BNode("Drugs");
-	nodeArray[3]->setArity(5);
-
-	nodeArray[4] = new BNode("Location At");
-	nodeArray[4]->setArity(3);
-
-	nodeArray[5] = new BNode("Location From");
-	nodeArray[5]->setArity(3);
-
-	nodeArray[6] = new BNode("Murder");
-	nodeArray[7] = new BNode("Robbery");
-	nodeArray[8] = new BNode("Rape");
-=======
 	nodeArray[1] = new BNode("Time of Day","timeOfDay");
 	nodeArray[1]->setArity(5);
 	nodeArray[2] = new BNode("Lighting","lighting");
@@ -47,8 +26,6 @@ int main(int argc, const char* argv[])
 	nodeArray[7] = new BNode("Robbery","robbery");
 
 	nodeArray[8] = new BNode("Rape","rape");
-
->>>>>>> updated project with data files
 
 	for(int i = 0; i < 6; i++)
 		for(int j = 6; j < 9; j++)
