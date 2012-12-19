4f4c884027d2990750ae34eba2f3f643a1239d98
Matthew Peveler <matt.peveler@gmail.com>
2012-12-18 19:10:18 -0500

    hardcoded example in project report and report

--- main.cpp
+++ main.cpp
@@ -53,8 +53,8 @@ int main(int argc, const char* argv[])
 
 	for(int i = 0; i < 10; i++)
 	{
-		cout << nodeArray[6]->getProbability(array) << endl;
-		array[5]++;
+		//cout << nodeArray[6]->getProbability(array) << endl;
+		//array[5]++;
 	}
 
 	array[0] = 2;
@@ -64,8 +64,53 @@ int main(int argc, const char* argv[])
 	array[4] = 3;
 	array[5] = 1;
 
-	cout << nodeArray[6]->getProbability(array) << endl;
-	cout << nodeArray[0]->getProbability(2) << endl;
+	//cout << nodeArray[6]->getProbability(array) << endl;
+	//cout << nodeArray[0]->getProbability(2) << endl;
+
+	// P = 2 | M = 1, D = 3, LocFrom = 1, LocAt = 2, T = 2
+
+	array[0] = 2; // Population
+	array[1] = 0; // Lighting
+	array[2] = 2; // Time of Day
+	array[3] = 3; // Drugs
+	array[4] = 2; // Location At
+	array[5] = 1; // Location From
+
+	double tmp = 1;
+	double top = 0;
+
+	// calculate top
+	for(int i = 0; i < nodeArray[1]->getArity(); i++) {
+		array[1] = i+1;
+		tmp = 1;
+		for(int j = 0; j < 6; j++) {
+			if(j == 1) continue;
+			tmp *= nodeArray[j]->getProbability(array[j]);
+		}
+		tmp *= nodeArray[6]->getProbability(array);
+		if(tmp != 1) top += tmp;
+	}
+
+	tmp = 1;
+	double bottom = 0;
+
+	// calculate bottom
+	for(int i = 0; i < nodeArray[0]->getArity(); i++) {
+		for(int j = 0; j < nodeArray[1]->getArity(); j++) {
+			array[0] = i+1;
+			array[1] = j+1;
+			tmp = 1;
+			for(int k = 0; k < 6; k++)
+			{
+				tmp *= nodeArray[k]->getProbability(array[k]);
+			}
+			tmp *= nodeArray[6]->getProbability(array);
+			if(tmp != 1) bottom += tmp;
+
+		}
+	}
+
+	cout << top << " / " << bottom << " = " << top/bottom << endl;
 
 	return 0;
 }
